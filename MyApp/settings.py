e540d04ace535b8cd2cd65a5c45f089e42a6999b
Thamizhvanan <thamizhpriyan.1995@gmail.com>
2019-02-21 10:12:50 +0530

    imported django_heroku

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 from corsheaders.middleware import ACCESS_CONTROL_ALLOW_CREDENTIALS,\
     ACCESS_CONTROL_ALLOW_ORIGIN
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
